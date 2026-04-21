74c52bb365f5fe3ee797829a1cc37ee30778df90
Soleguard <139895712+Soleguard@users.noreply.github.com>
2026-04-21 07:30:22 -0400

    Included main and comments for click/key input events, win/loss conditions, and end game screen

--- sudoku.py
+++ sudoku.py
@@ -1,60 +1,100 @@
 # Import pygame and sys modules
 import pygame
-# import sys
+import sys
 
-# Import Board and SudokuGenerator classes and start_screen function
+# Import Board class and start_screen and generate_sudoku functions
 from board import Board
-from sudoku_generator import SudokuGenerator
 from board import start_screen
+from sudoku_generator import generate_sudoku
 
-# Initialize pygame
-pygame.init()
+# Main
+if __name__ == "__main__":
 
-# Screen display
-screen_display = pygame.display
+    # Initialize pygame
+    pygame.init()
 
-# Form screen
-screen = screen_display.set_mode()
+    # Screen display
+    screen_display = pygame.display
 
-# Get default size
-x, y = screen.get_size()
+    # Form screen
+    screen = screen_display.set_mode()
 
-# Store screen size
-z = [x, y]
+    # Get default size
+    x, y = screen.get_size()
 
-# Set size of window
-win = screen_display.set_mode(z)
+    # Store screen size
+    z = [x, y]
 
-# Difficulty
-difficulty = start_screen(win)
+    # Set size of window
+    win = screen_display.set_mode(z)
 
-# Determine number of removed cells
-if difficulty == "easy":
-    removed_cells = 30
+    # Difficulty
+    difficulty = start_screen(win)
 
-elif difficulty == "medium":
-    removed_cells = 40
+    # Determine number of removed cells
+    if difficulty == "easy":
+        removed_cells = 30
 
-elif difficulty == "hard":
-    removed_cells = 50
+    elif difficulty == "medium":
+        removed_cells = 40
 
-# Board data
-board_data = SudokuGenerator.generate_sudoku(9, removed_cells)
+    elif difficulty == "hard":
+        removed_cells = 50
 
-# Call Board class from board [dot] py
-board = Board(540, 540, win, difficulty, board_data)
+    # Board data
+    board_data = generate_sudoku(9, removed_cells)
 
-# Loop through Sudoku game
-while True:
+    # Call Board class from board [dot] py
+    board = Board(540, 540, win, difficulty, board_data)
 
-    board.draw()
+    # Loop through Sudoku game
+    while True:
 
-    # # Event loop
-    # for event in pygame.event.get():
-    #
-    #     # Manage player exit
-    #     if event.type == pygame.QUIT:
-    #         pygame.quit()
-    #         sys.exit()
+        board.draw()
 
-    pygame.display.update()
+        # Event loop
+        for event in pygame.event.get():
+
+            # Manage player exit
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            # Click event
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+                # Get position
+                # x, y = event.pos
+                #
+                # # Check if player selects 1 - Easy
+                # if (x == 180) and (y == 250):
+                #     print("Generate Sudoku Board")
+
+            # Key input event
+            # if event.type == pygame.KEYDOWN:
+            #     if event.key == pygame.K_1:
+            #         # 1
+            #     elif event.key == pygame.K_2:
+            #         # 2
+            #     elif event.key == pygame.K_3:
+            #         # 3
+            #     elif event.key == pygame.K_4:
+            #         # 4
+            #     elif event.key == pygame.K_5:
+            #         # 5
+            #     elif event.key == pygame.K_6:
+            #         # 6
+            #     elif event.key == pygame.K_7:
+            #         # 7
+            #     elif event.key == pygame.K_8:
+            #         # 8
+            #     elif event.key == pygame.K_9:
+            #         # 9
+            #     # Return/enter key
+            #     elif event.key == pygame.K_RETURN:
+            #         # Return
+
+            # Win/loss condition
+
+            # End game screen
+
+        pygame.display.update()
